6393ee97df187fb50f492e6d1f5f20c0967b0501
Somya Bhatt <somyacu1@gmail.com>
2024-02-06 17:55:59 +0530

    :modified, added :re-usable fn

--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,62 @@
 //? this file is just for testing purpose
-const { exec } = require('child_process')
 const fs = require('fs');
-
 import { promisify } from 'util';
+import * as child_process from 'child_process';
+import { PowerShellCommandOutput,TrackFile } from '../types/index';
 
-// types
-import { ExecException } from "child_process";
-import {
-    PowerShellCommandOutput,
-    TrackFile
-} from '../types/index';
 
-// paths
+//? paths
 const outputFilePath = './json/output.json';
 const trackFilePath = './json/track.json';
 const detailsFilePath = './json/details.json';
 const psFilePath = './scripts/index.ps1';
 
-console.log("Getting started\n");
+//? functions
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const exec = promisify(child_process.exec);
 
- function runPowerShellCommand() {
-    exec(`powershell.exe -File ${psFilePath}`, async (err: ExecException, stdout: string, stderr: string) => {
 
-        if (err) {
-            throw err;
-        }
-
+ async function runPowerShellCommand() {
+    try {
+        const { stdout } = await exec(`powershell.exe -File ${psFilePath}`);
         const output = JSON.parse(stdout);
+        await writeFile(outputFilePath, JSON.stringify(output));
+        console.log(`Successfully Written to ${outputFilePath}`);
+    } catch (error) {
+        console.error(`Error executing PowerShell command: ${error}`);
+    }
 
-        fs.writeFile(outputFilePath, JSON.stringify(output), (err: any) => {
-            if (err) {
-                console.log(err);
-            }
-            console.log(`Successfully Written to ${outputFilePath}`);
-        });
-    });
 }
 
-
-function readOutput() : PowerShellCommandOutput[]{
-    try{
-        const rawdata = fs.readFileSync(outputFilePath,'utf8');
-        if(!rawdata){
-            return [];
-        }
-        const output = JSON.parse(rawdata);
-        return output;
-    } catch(err){
-        console.log("Err in reading output.json",err);
+async function readJsonFile(filePath: string) {
+    try {
+        const rawdata = await readFile(filePath, 'utf8');
+        return JSON.parse(rawdata);
+    } catch (err) {
+        console.log(`Error reading ${filePath}`, err);
         return [];
     }
 }
 
-function readTrackFile() : TrackFile[]{
-    try{
-        const rawdata = fs.readFileSync(trackFilePath,'utf8');
-        if(!rawdata){
-            return [];
-        }
-        const output = JSON.parse(rawdata);
-        return output;
-    } catch(err){
-        console.log("Err in reading track.json",err);
-        return [];
-    }
-}
-
-function readDetailsFile() : TrackFile[]{
-    try{
-        const rawdata = fs.readFileSync(detailsFilePath,'utf8');
-        if(!rawdata){
-            return [];
-        }
-        const output = JSON.parse(rawdata);
-        return output;
-    } catch(err){
-        console.log("Err in reading details.json",err);
-        return [];
+async function writeJsonFile(filePath: string, data: any) {
+    try {
+        await writeFile(filePath, JSON.stringify(data));
+        console.log(`Successfully written to ${filePath}`);
+    } catch (err) {
+        console.log(`Error writing to ${filePath}`, err);
     }
 }
 
 
-function trackUsage(){
-    const currentProcess = readOutput();
+async function trackUsage(){
+    const currentProcess = await readJsonFile(outputFilePath);
 
     if (!currentProcess){
         throw new Error('Current process not found');
     }
 
-    const trackFile = readTrackFile();
-    // console.log("trackFile",trackFile);
+    const trackFile = await readJsonFile(trackFilePath);
 
     //? if track.json is empty then add the entry in track.json for every process in currentProcess
     if(trackFile.length === 0){
@@ -104,41 +71,26 @@ function trackUsage(){
             }
             return newTrackFileEntry;
         });
-
-  
-        fs.writeFile(trackFilePath, JSON.stringify(newTrackFile), (err: any) => {
-            if (err) {
-                console.log("Err in writing into trackFile",err);
-            }
-            console.log(`Successfully Written Entry in ${trackFilePath}\n`);
-        });
+        await writeJsonFile(trackFilePath, newTrackFile);
 
         return;
     }
 
-    // check if every process in track.json is running or not
-    // if not running then remove the entry from track.json
-    // if running then update the entry in track.json
-    // ?how do i check for every process in track.json is running or not 
-    // ?if not running then remove the entry from track.json for that particular process 
-    // check for every process in track.json is running or not and return process which are not running
-
-
-    const checkForEveryProcess = trackFile.every((ele) => {
-        return currentProcess.some((ele2) => {
+    const checkForEveryProcess = trackFile.every((ele : TrackFile) => {
+        return currentProcess.some((ele2 : PowerShellCommandOutput) => {
             return ele.Id === ele2.Id;
         });
     })
 
     //? check for new process in currentProcess
 
-    const newProcess = currentProcess.filter((ele) => {
-        return !trackFile.some((ele2) => {
+    const newProcess = currentProcess.filter((ele : PowerShellCommandOutput) => {
+        return !trackFile.some((ele2 : TrackFile) => {
             return ele.Id === ele2.Id;
         });
     });
 
-    //? track the new process   -- there might be a case where the process is not running but there is an entry in track.json
+    //? track the new process   -- there might be a case where the process is not      but there is an entry in track.json
     if(newProcess.length > 0){
         const newTrackFile = newProcess.map((ele : PowerShellCommandOutput) => {
             const newTrackFileEntry : TrackFile = {
@@ -152,12 +104,7 @@ function trackUsage(){
             return newTrackFileEntry;
         });
 
-        fs.writeFile(trackFilePath, JSON.stringify([...trackFile, ...newTrackFile]), (err: any) => {
-            if (err) {
-                console.log("Err in writing into trackFile",err);
-            }
-            console.log(`Successfully Written Entry in ${trackFilePath}\n`);
-        });
+        await writeJsonFile(trackFilePath, [...trackFile, ...newTrackFile]);
 
         return;
     }
@@ -177,35 +124,33 @@ function trackUsage(){
             }
             return newTrackFileEntry;
         });
-
+        
         console.log("newTrackFile",newTrackFile);
-
-        fs.writeFile(trackFilePath, JSON.stringify(newTrackFile), (err: any) => {
-            if (err) {
-                console.log("Err in writing into trackFile",err);
-            }
-            console.log(`Successfully Written Entry in ${trackFilePath}`);
-        });
+        await writeJsonFile(trackFilePath, newTrackFile);
 
         return; //? is this return necessary
     }
 
+    //? 
+
+
+
     //? if every process is not running than remove the entry from track.json
     const newTrackFile = trackFile.filter((ele : TrackFile) => {
-        return currentProcess.some((ele2) => {
+        return currentProcess.some((ele2 : PowerShellCommandOutput) => {
             return ele.Id === ele2.Id;
         });
     });
 
     //? console the expected run time for the process which is not running
     const notRunningProcess = trackFile.filter((ele : TrackFile) => {
-        return !currentProcess.some((ele2) => {
+        return !currentProcess.some((ele2 : PowerShellCommandOutput) => {
             return ele.Id === ele2.Id;
         });
     });
 
     //? reading the details.json
-    const detailsFile = readDetailsFile();
+    const detailsFile = await readJsonFile(detailsFilePath);
 
     const newNotRunningProcess = notRunningProcess.map((ele : TrackFile) => {
         const newDetailsFileEntry : TrackFile = {
@@ -220,146 +165,46 @@ function trackUsage(){
         return newDetailsFileEntry;
     });
 
-    //? updating the details.json
     const newDetailsFile = [...detailsFile, ...newNotRunningProcess];
+    await writeJsonFile(detailsFilePath, newDetailsFile);
 
 
-    //? writing the notRunningProcess to details.json
-    fs.writeFile(detailsFilePath, JSON.stringify(newDetailsFile), (err: any) => {
-        if (err) {
-            console.log("Err in writing into detailsFile",err);
-        }
-        console.log(`Successfully Written Entry in ${detailsFilePath}`);
-    });
-
-
-    notRunningProcess.forEach((ele) => {
+    notRunningProcess.forEach((ele : TrackFile) => {
         console.log(`Process ${ele.ProcessName} is not running && expected run time is ${ele.totalTimes} seconds\n`);
     });
-
-    //? write to file
-
-    fs.writeFile(trackFilePath, JSON.stringify(newTrackFile), (err: any) => {
-        if (err) {
-            console.log("Err in updating",err);
-        }
-        console.log(`Successfully Updated Entry in ${trackFilePath}`);
-    });
-
+    await writeJsonFile(trackFilePath, newTrackFile);
     return;
-
-    //? is everything handled in above code
-
-    //todo : check if something is missing
-
-
-    //?rough-start for an single process (chrome)
-    // console.log("isChromePresent",isChromePresent)
-    // const path = './track.json';
-
-    // const isChromeRunning = currentProcess.some((ele : PowerShellCommandOutput) => {
-    //     return ele.ProcessName.toLowerCase() === 'chrome';
-    // });
-
-    // console.log("isChromeRunning",isChromeRunning);
-    //     if(isChromePresent && isChromeRunning){
-    //         let chromeEntry = trackFile.find((ele : TrackFile) => {
-    //             return ele.ProcessName.toLowerCase() === 'chrome';
-    //         });
-
-    //         console.log("chromeEntry",chromeEntry);
-    //         // update the entry in track.json
-
-    //         if(!chromeEntry){
-    //             throw new Error('Chrome entry not found');
-    //         }
-
-    //         chromeEntry.totalTimes += 1;
-
-    //         // write to file
-
-    //         fs.writeFile(path, JSON.stringify(trackFile), (err: any) => {
-    //             if (err) {
-    //                 console.log("Err in updating",err);
-    //             }
-    //             console.log(`Successfully Updated Entry in ${path}`);
-    //         });
-
-    //         return;
-
-    //     }
-
-        // if chrome is not running & also there is an entry in track.json
-        // console the time and remove the entry from track.json
-
-        // if(isChromePresent && !isChromeRunning){
-
-        //     let chromeEntry = trackFile.find((ele : TrackFile) => {
-        //         return ele.ProcessName.toLowerCase() === 'chrome';
-        //     });
-
-        //     console.log("chromeEntry",chromeEntry);
-        //     // update the entry in track.json
-
-        //     if(!chromeEntry){
-        //         throw new Error('Chrome entry not found');
-        //     }
-
-        //     console.log(`Chrome is closed && expected run time is ${chromeEntry.totalTimes} seconds`);
-
-        //     // remove the entry from track.json
-        //     const newTrackFile = trackFile.filter((ele : TrackFile) => {
-        //         return ele.ProcessName.toLowerCase() !== 'chrome';
-        //     });
-
-        //     // write to file
-        //     fs.writeFile(path, JSON.stringify(newTrackFile), (err: any) => {
-        //         if (err) {
-        //             console.log("Err in updating",err);
-        //         }
-        //         console.log(`Successfully Removed Entry in ${path}`);
-        //     });
-
-        //     return;
-        // }
-
-        // if chrome is running & there is no entry in track.json
-        // add the entry in track.json
-
-        // if(!isChromePresent && isChromeRunning){
-
-        //     const chromeEntry : TrackFile = {
-        //         ProcessType : 'FOREGROUND',
-        //         ProcessName : 'chrome',
-        //         Id : currentProcess[0].Id,
-        //         totalTimes : 1
-        //     }
-
-        //     fs.writeFile(path, JSON.stringify([...trackFile, chromeEntry]), (err: any) => {
-        //         if (err) {
-        //             console.log("Err in writing into trackFile",err);
-        //         }
-        //         console.log(`Successfully Written Entry in ${path}`);
-        //     });
-
-        //     return;
-        // }
-
-        // if chrome is not running & there is no entry in track.json
-
-        // if(!isChromePresent && !isChromeRunning){
-        //     return;
-        // }
 
         //?rough-end
 }
 
 
-//? running the powershell command every 1 second (just for testing)
+function clearFile(filePath: string) {
+    fs.writeFileSync(filePath, JSON.stringify([]));
+}
 
-// runPowerShellCommand();
+process.on('exit', () => {
+    clearFile(outputFilePath);
+    clearFile(trackFilePath);
+    clearFile(detailsFilePath);
+});
 
-setInterval(() => {
-    runPowerShellCommand();
-    trackUsage();
-}, 5000);
+process.on('SIGINT', () => {
+    clearFile(outputFilePath);
+    clearFile(trackFilePath);
+    clearFile(detailsFilePath);
+    process.exit();
+});
+
+
+async function main(){
+    try {
+        await runPowerShellCommand();
+        await trackUsage();
+    } catch (err) {
+        console.log(err);
+    }
+    setTimeout(main, 5000);
+}
+
+main();
